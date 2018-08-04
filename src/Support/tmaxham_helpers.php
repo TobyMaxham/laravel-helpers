a5c7b1e0f5c15d6f078c27a4730951db65d9fd45
TobyMaxham <git2018@maxham.de>
2018-08-04 10:45:34 +0200

    Added a helper file with functions

---
+++ src/Support/tmaxham_helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!function_exists('_val')) {
+    /**
+     * @param $value
+     * @param null $default
+     * @return mixed
+     */
+    function _val($value, $default = null)
+    {
+        return is_null($value) ? value($default) : $value;
+    }
+}
